54b0f001ef170656a458f0f980f1e096d16832d9
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-02 12:15:06 -0400

    add Xcode project

---
+++ Quakes/Quakes/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Quakes
+//
+//  Created by Julian A. Fordyce on 4/2/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ Quakes/Quakes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quakes
+//
+//  Created by Julian A. Fordyce on 4/2/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
