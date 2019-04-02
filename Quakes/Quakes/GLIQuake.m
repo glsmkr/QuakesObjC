373e8afd81fed2e6af37dd32c112f53e9dd943f3
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-02 12:16:19 -0400

    add nsobject file for model

---
+++ Quakes/Quakes/GLIQuake.m
@@ -0,0 +1,13 @@
+//
+//  GLIQuake.m
+//  Quakes
+//
+//  Created by Julian A. Fordyce on 4/2/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "GLIQuake.h"
+
+@implementation GLIQuake
+
+@end
